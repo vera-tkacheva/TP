4e4bc3828ad63f2b5012ed9b4d2a29f616f3a7b1
vera-tkacheva <167525612+vera-tkacheva@users.noreply.github.com>
2024-05-03 11:55:24 +0300

    Update tz1.sh + counter

--- TZ1/tz1.sh
+++ TZ1/tz1.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Получаем на вход 2 директории
+if [[ $# -ne 2 ]]; then
+    echo "Требуется ввести: $0 input_dir output_dir"
+    exit 1
+fi
+
 input_dir="$1"
 output_dir="$2"
 
 # Предоставляем права на чтение и выполнение для входной директории и ее содержимого
 chmod -R +xr "$input_dir"
+
 
 files_in_input_dir=()
 subdirs=()
@@ -24,7 +30,6 @@ done
 # Добавляем в соответствующий массив файлы из каждой поддиректории
 for subdir in "${subdirs[@]}"; do
     for file in "$subdir"/*; do
-        # Если файл, то добавляем в массив файлов
         if [[ -f "$file" ]]; then
             files_in_input_dir+=("$file")
         fi
@@ -33,15 +38,21 @@ done
 
 # Копируем файлы из массива в выходную директорию
 for file in "${files_in_input_dir[@]}"; do
-    real_path=$(realpath "$file")
-    file_name=$(basename "$real_path")
-    # Если файл с таким именем уже есть в выходной директории, то добавляем к имени хэш-код
-    if [ -e "$output_dir/$file_name" ]; then
-        hash_code=$(md5 "$real_path" | cut -d ' ' -f 1)
-        new_file_name="${file_name%.*}_$hash_code.${file_name##*.}"
-        cp "$real_path" "$output_dir/$new_file_name"
-    # Если файла с таким именем нет, то оставляем как есть
-    else
-        cp "$real_path" "$output_dir/$file_name"
-    fi
+    file_name=$(basename -- "$file")
+    end_file_path="$output_dir/$file_name"
+    counter=1
+    # Пока существует файл с таким именем в выходной директории, увеличиваем счетчик; добавляем его к названию
+    while [ -e "$end_file_path" ]; do
+        name="${file_name%.*}"
+        extension="${file_name##*.}"
+        # Если файл содежит точку (у него есть расширение), то к имени добавляем счетчик и расширение
+        if [[ "$file_name" == *.* ]]; then
+            end_file_path="$output_dir/${name}_$counter.$extension"
+        # Если расширения нет, то к имени добавляем только счетчик
+        else
+            end_file_path="$output_dir/${name}_$counter"
+        fi
+        ((counter++))
+    done
+    cp "$file" "$end_file_path"
 done
